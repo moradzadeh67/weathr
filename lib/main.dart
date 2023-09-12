05e4bb0422651971649eb5dc961572b195ecb5a9
moradzadeh67 <moradzade.mohamadreza@gmail.com>
2023-09-11 21:47:23 -0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class _MyAppState extends State<MyApp> {
                         padding: const EdgeInsets.only(top: 5),
                         child: Text(
                           '16' + '\u00B0',
-                          style: TextStyle(color: Colors.grey, fontSize: 20),
+                          style: TextStyle(color: Colors.white, fontSize: 20),
                         ),
                       ),
                     ],
@@ -88,13 +88,153 @@ class _MyAppState extends State<MyApp> {
                           padding: const EdgeInsets.only(top: 5),
                           child: Text(
                             '12' + '\u00B0',
-                            style: TextStyle(color: Colors.grey, fontSize: 20),
+                            style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
                         ),
                       ],
                     ),
                   ),
                 ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: Container(
+                  color: Colors.grey,
+                  height: 1,
+                  width: double.infinity,
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 100,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Center(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 6,
+                      itemBuilder: (BuildContext context, int pos) {
+                        return Container(
+                          height: 50,
+                          width: 70,
+                          child: Card(
+                            color: Colors.transparent,
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Fri 8PM',
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 15),
+                                ),
+                                Icon(
+                                  Icons.cloud,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  '14' + '\u00B0',
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 20),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: Container(
+                  color: Colors.grey,
+                  height: 1,
+                  width: double.infinity,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10,),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Text('Wind Speed', style: TextStyle(color: Colors.white),),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Text('4.73 m/s', style: TextStyle(color: Colors.white),),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Container(
+                        child: Container(
+                        width: 1,
+                        height: 40,
+                        color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Column(
+                        children: [
+                          Text('Sunrise', style: TextStyle(color: Colors.white),),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Text('6:19 PM', style: TextStyle(color: Colors.white),),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Container(
+                        child: Container(
+                          width: 1,
+                          height: 40,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Column(
+                        children: [
+                          Text('Sunset', style: TextStyle(color: Colors.white),),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Text('9:30 AM', style: TextStyle(color: Colors.white),),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Container(
+                        child: Container(
+                          width: 1,
+                          height: 40,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Column(
+                        children: [
+                          Text('Humidity', style: TextStyle(color: Colors.white),),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Text('72%', style: TextStyle(color: Colors.white),),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               )
             ],
           ),
